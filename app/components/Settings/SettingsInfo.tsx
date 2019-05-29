34dd64874f571449d61a748d4f3fd5018863a14a
Aleksandr Gavrilov <alg@digt.ru>
2019-05-29 15:03:32 +0300

    show settings info

---
+++ app/components/Settings/SettingsInfo.tsx
@@ -0,0 +1,134 @@
+import PropTypes from "prop-types";
+import React from "react";
+import RecipientsList from "../RecipientsList";
+
+interface ISettingsInfoProps {
+  setting: any;
+}
+
+export default class SettingsInfo extends React.Component<ISettingsInfoProps, {}> {
+  static contextTypes = {
+    locale: PropTypes.string,
+    localize: PropTypes.func,
+  };
+
+  render() {
+    const { localize, locale } = this.context;
+    const { recipients, setting, signer } = this.props;
+
+    return (
+      <div className="add-cert-collection collection cert-info-list">
+        <div className="collection-item certs-collection certificate-info">
+          <div className="collection-info cert-info-blue">
+            {localize("Settings.name", locale)}
+          </div>
+          <div className="collection-title selectable-text">
+            {setting.name}
+          </div>
+        </div>
+
+        <div className="collection-item certs-collection certificate-info">
+          <div className="collection-info cert-info-blue">
+            {localize("Documents.save_to_documents", locale)}
+          </div>
+          <div className="collection-title selectable-text">
+            {setting.sign.saveToDocuments ? localize("Common.yes", locale) : localize("Common.no", locale)}
+          </div>
+        </div>
+
+        <div className="collection-item certs-collection certificate-info">
+          <div className="collection-info cert-info-blue">
+            {localize("Sign.sign_detached", locale)}
+          </div>
+          <div className="collection-title selectable-text">
+            {setting.sign.detached ? localize("Common.yes", locale) : localize("Common.no", locale)}
+          </div>
+        </div>
+
+        <div className="collection-item certs-collection certificate-info">
+          <div className="collection-info cert-info-blue">
+            {localize("Sign.sign_time", locale)}
+          </div>
+          <div className="collection-title selectable-text">
+            {setting.sign.timestamp ? localize("Common.yes", locale) : localize("Common.no", locale)}
+          </div>
+        </div>
+
+        <div className="collection-item certs-collection certificate-info">
+          <div className="collection-info cert-info-blue">
+            {"Сертификат подписи:"}
+          </div>
+          {(signer) ? this.getSelectedSigner() :
+            <div className="collection-title selectable-text">
+              {localize("Common.no", locale)}
+            </div>}
+        </div>
+
+        <div className="collection-item certs-collection certificate-info">
+          <div className="collection-info cert-info-blue">
+            {localize("Encrypt.delete_files_after", locale)}
+          </div>
+          <div className="collection-title selectable-text">
+            {setting.encrypt.delete ? localize("Common.yes", locale) : localize("Common.no", locale)}
+          </div>
+        </div>
+
+        <div className="collection-item certs-collection certificate-info">
+          <div className="collection-info cert-info-blue">
+            {localize("Encrypt.archive_files_before", locale)}
+          </div>
+          <div className="collection-title selectable-text">
+            {setting.encrypt.archive ? localize("Common.yes", locale) : localize("Common.no", locale)}
+          </div>
+        </div>
+
+        <div className="collection-item certs-collection certificate-info">
+          <div className="collection-info cert-info-blue">
+            {"Сертификаты шифрования:"}
+          </div>
+          {(recipients && recipients.length) ?
+            <div style={{ height: "calc(100vh - 490px)", fontSize: "125%" }}>
+              <div className="add-certs">
+                <RecipientsList recipients={recipients} />
+              </div>
+            </div> :
+            <div className="collection-title selectable-text">
+              {localize("Common.no", locale)}
+            </div>}
+        </div>
+      </div>
+    );
+  }
+
+  getSelectedSigner = () => {
+    const { signer } = this.props;
+    const { localize, locale } = this.context;
+
+    if (signer) {
+      const status = signer.status;
+      let curStatusStyle;
+
+      if (status) {
+        curStatusStyle = "cert_status_ok";
+      } else {
+        curStatusStyle = "cert_status_error";
+      }
+
+      return (
+        <React.Fragment>
+          <div className="col s12 valign-wrapper">
+            <div className="col s2">
+              <div className={curStatusStyle} />
+            </div>
+            <div className="col s10">
+              <div className="collection-title">{signer.subjectFriendlyName}</div>
+              <div className="collection-info cert-info">{signer.issuerFriendlyName}</div>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+    } else {
+      return null;
+    }
+  }
+}
--- app/components/Settings/SettingsWindow.tsx
+++ app/components/Settings/SettingsWindow.tsx
@@ -8,6 +8,8 @@ import {
 import {
   LOCATION_SETTINGS_CONFIG,
 } from "../../constants";
+import { mapToArr } from "../../utils";
+import SettingsInfo from "./SettingsInfo";
 import SettingsTable from "./SettingsTable";
 
 interface ISettingsWindowState {
@@ -39,13 +41,11 @@ class SettingsWindow extends React.Component<{}, ISettingsWindowState> {
       inDuration: 300,
       outDuration: 225,
     });
-
-    this.props.activeSetting(this.props.settings.default);
   }
 
   render() {
     const { localize, locale } = this.context;
-    const { setting, settings } = this.props;
+    const { recipients, setting, settings, signer } = this.props;
 
     return (
       <div className="content-noflex">
@@ -62,51 +62,54 @@ class SettingsWindow extends React.Component<{}, ISettingsWindowState> {
             <div className="row" />
             {
               setting ?
-                <div className="row fixed-bottom-rightcolumn" style={{ bottom: "20px" }}>
-                  <div className="col s12">
-                    <hr />
-                  </div>
-
-                  {
-                    settings.default !== setting.id ?
-                      <div className="col s4 waves-effect waves-cryptoarm">
-                        <div className="col s12 svg_icon">
-                          <a onClick={() => this.props.changeDefaultSettings(setting.id)}
-                            data-position="bottom">
-                            <i className="material-icons certificate export" />
-                          </a>
-                        </div>
-                        <div className="col s12 svg_icon_text">{"Применить"}</div>
-                      </div>
-                      :
-                      null
-                  }
-
-                  <div className="col s4 waves-effect waves-cryptoarm">
-                    <div className="col s12 svg_icon">
-                      <a onClick={() => this.props.history.push(LOCATION_SETTINGS_CONFIG)}
-                        data-position="bottom">
-                        <i className="material-icons certificate export" />
-                      </a>
+                <React.Fragment>
+                  <SettingsInfo setting={setting} signer={signer} recipients={recipients} />
+                  <div className="row fixed-bottom-rightcolumn" style={{ bottom: "20px" }}>
+                    <div className="col s12">
+                      <hr />
                     </div>
-                    <div className="col s12 svg_icon_text">{"Редактирвоать"}</div>
-                  </div>
 
-                  {
-                    settings.default !== setting.id ?
-                      <div className="col s4 waves-effect waves-cryptoarm">
-                        <div className="col s12 svg_icon">
-                          <a onClick={() => this.props.deleteSetting(setting.id)}
-                            data-position="bottom">
-                            <i className="material-icons certificate remove" />
-                          </a>
+                    {
+                      settings.default !== setting.id ?
+                        <div className="col s4 waves-effect waves-cryptoarm">
+                          <div className="col s12 svg_icon">
+                            <a onClick={() => this.props.changeDefaultSettings(setting.id)}
+                              data-position="bottom">
+                              <i className="material-icons certificate export" />
+                            </a>
+                          </div>
+                          <div className="col s12 svg_icon_text">{"Применить"}</div>
                         </div>
-                        <div className="col s12 svg_icon_text">{localize("Documents.docmenu_remove", locale)}</div>
-                      </div> :
-                      null
-                  }
+                        :
+                        null
+                    }
 
-                </div>
+                    <div className="col s4 waves-effect waves-cryptoarm">
+                      <div className="col s12 svg_icon">
+                        <a onClick={() => this.props.history.push(LOCATION_SETTINGS_CONFIG)}
+                          data-position="bottom">
+                          <i className="material-icons certificate export" />
+                        </a>
+                      </div>
+                      <div className="col s12 svg_icon_text">{"Редактирвоать"}</div>
+                    </div>
+
+                    {
+                      settings.default !== setting.id ?
+                        <div className="col s4 waves-effect waves-cryptoarm">
+                          <div className="col s12 svg_icon">
+                            <a onClick={() => this.props.deleteSetting(setting.id)}
+                              data-position="bottom">
+                              <i className="material-icons certificate remove" />
+                            </a>
+                          </div>
+                          <div className="col s12 svg_icon_text">{localize("Documents.docmenu_remove", locale)}</div>
+                        </div> :
+                        null
+                    }
+
+                  </div>
+                </React.Fragment>
                 :
                 <div className="row fixed-bottom-rightcolumn" style={{ bottom: "20px" }}>
                   <div className="col s12">
@@ -140,6 +143,10 @@ class SettingsWindow extends React.Component<{}, ISettingsWindowState> {
 }
 
 export default connect((state) => ({
+  recipients: mapToArr(state.recipients.entities)
+    .map((recipient) => state.certificates.getIn(["entities", recipient.certId]))
+    .filter((recipient) => recipient !== undefined),
   setting: state.settings.getIn(["entities", state.settings.active]),
   settings: state.settings,
+  signer: state.certificates.getIn(["entities", state.signers.signer]),
 }), { activeSetting, changeDefaultSettings, createSettings, deleteSetting })(SettingsWindow);
