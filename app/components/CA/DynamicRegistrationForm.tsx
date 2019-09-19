70f968f1b9875c71db7d0a983301da21b7fe2501
Aleksandr Gavrilov <alg@digt.ru>
2019-09-19 11:47:42 +0300

    add support selected input
    https://git.digtlab.ru/trusted.ru/cryptoarmgost/issues/168

--- app/components/CA/DynamicRegistrationForm.tsx
+++ app/components/CA/DynamicRegistrationForm.tsx
@@ -1,8 +1,50 @@
 import PropTypes from "prop-types";
 import React from "react";
+import ReactDOM from "react-dom";
+
+interface IRDNObject {
+  /**
+   * Значение OIDэлемента учетной записи пользователя
+   */
+  Oid: string;
+  /**
+   * Наименование OIDэлемента учетной записи пользователя
+   */
+  Name: string;
+  /**
+   * Локализованное наименование OIDэлемента учетной записи пользователя
+   */
+  LocalizedName: string;
+  /**
+   * Список возможных значений элемента учетной записи пользователя
+   */
+  SettingsValues: string[];
+  /**
+   * Значение по умолчанию элемента учетной записи пользователя из списка SettingValues
+   */
+  DefaultValue: string;
+  /**
+   * Флаг указывающий, что значение элемента учетной записи входит в список значений SettingsValues
+   */
+  ProhibitAnyValue: boolean;
+  /**
+   * Флаг указывающий, что элемент учетной записи не может быть изменен
+   */
+  ProhibitChange: boolean;
+  /**
+   * Флаг указывающий, что элемент учетной записи должен быть непустым
+   */
+  ProhibitEmpty: boolean;
+}
 
 interface IDynamicRegistrationFormProps {
-  model: any;
+  /**
+   * Значения элементов учетных записей пользователейорганизованных в папке УЦ
+   *
+   * @type {IRDNObject[]}
+   * @memberof IDynamicRegistrationFormProps
+   */
+  RDN: IRDNObject[];
 }
 
 interface IDynamicRegistrationFormState {
@@ -18,10 +60,22 @@ class DynamicRegistrationForm extends React.Component<IDynamicRegistrationFormPr
   constructor(props: any) {
     super(props);
 
-    this.state = {};
+    const model: any = {};
+
+    props.RDN.map((field: IRDNObject) => model[field.Oid] = field.DefaultValue);
+
+    this.state = { ...model };
   }
 
   componentDidMount() {
+    const self = this;
+
+    $(document).ready(() => {
+      $("select").material_select();
+
+      $("select").on("change", self.handleInputChange);
+    });
+
     Materialize.updateTextFields();
   }
 
@@ -31,28 +85,57 @@ class DynamicRegistrationForm extends React.Component<IDynamicRegistrationFormPr
 
   render() {
     const { localize, locale } = this.context;
-    const { model } = this.props;
+    const { RDN } = this.props;
 
     return (
       <div className="row">
         {
-          model.map((field: any) => {
-            return (
-              <div className="row">
-                <div className="input-field input-field-csr col s12">
-                  <input
-                    id={field.Oid}
-                    type="text"
-                    className="validate"
-                    name={field.Oid}
-                    value={field.DefaultValue}
-                    onChange={this.handleInputChange}
-                    placeholder={`${field.LocalizedName} (oid: ${field.Oid})`}
-                  />
-                  <label htmlFor={field.Oid}>{field.LocalizedName}</label>
+          RDN.map((field: IRDNObject) => {
+            if (field.SettingsValues && field.SettingsValues.length) {
+              return (
+                <div className="row">
+                  <div className="input-field input-field-csr col s12">
+                    <select
+                      disabled={field.ProhibitChange}
+                      id={field.Oid}
+                      className="select"
+                      name={field.Oid}
+                      value={field.DefaultValue}
+                      onChange={this.handleInputChange}
+                    >
+                      {
+                        field.SettingsValues.map((settingsValue) =>
+                          <option value={settingsValue}>
+                            {settingsValue}
+                          </option>)
+                      }
+                    </select>
+
+                    <label htmlFor={field.Oid}>{field.LocalizedName} {field.ProhibitEmpty ? " *" : ""}</label>
+                  </div>
                 </div>
-              </div>
-            );
+              );
+            } else {
+              const oidValue = this.state[field.Oid];
+
+              return (
+                <div className="row">
+                  <div className="input-field input-field-csr col s12">
+                    <input
+                      disabled={field.ProhibitChange}
+                      id={field.Oid}
+                      type="text"
+                      className="validate"
+                      name={field.Oid}
+                      value={oidValue}
+                      onChange={this.handleInputChange}
+                      placeholder={`${field.LocalizedName} (oid: ${field.Oid})`}
+                    />
+                    <label htmlFor={field.Oid}>{field.LocalizedName} {field.ProhibitEmpty ? " *" : ""}</label>
+                  </div>
+                </div>
+              );
+            }
           })
         }
       </div>
