b6cdb4a1160d29a538fd246136ccf78388345797
DmitryKatelinsky <52447079+DmitryKatelinsky@users.noreply.github.com>
2019-10-11 16:23:13 +0300

    add deletecontainer and certificate

--- app/components/Containers/ContainerDelete.tsx
+++ app/components/Containers/ContainerDelete.tsx
@@ -3,24 +3,50 @@ import React from "react";
 import { USER_NAME } from "../../constants";
 import logger from "../../winstonLogger";
 
+interface ICertificateDeleteState {
+  certificate: string;
+  deleteCertificate: boolean;
+}
+
 interface IContainerDeleteProps {
   container: any;
   onCancel?: () => void;
   reloadContainers: () => void;
 }
 
-class ContainerDelete extends React.Component<IContainerDeleteProps, {}> {
+class ContainerDelete extends React.Component<IContainerDeleteProps,ICertificateDeleteState, {}> {
   static contextTypes = {
     locale: PropTypes.string,
     localize: PropTypes.func,
   };
+
+  constructor(props: IContainerDeleteProps) {
+    super(props);
+
+    this.state = ({
+      certificate: "",
+      deleteCertificate: false,
+    });
+  }
+
+  componentDidMount() {
+    const { container } = this.props;
+
+    if (container) {
+      const certificate = this.getCertificateByContainer(container);
+      this.setState({ certificate });
+    }
+  }
 
   componentWillUnmount() {
     this.handelCancel();
   }
 
   render() {
+    const { certificate, deleteCertificate } = this.state;
     const { localize, locale } = this.context;
+    const { container, reloadContainers } = this.props;
+    console.log(certificate)
 
     return (
       <React.Fragment>
@@ -28,10 +54,24 @@ class ContainerDelete extends React.Component<IContainerDeleteProps, {}> {
           <div className="col s12">
             <div className="content-wrapper tbody border_group">
               <div className="col s12">
+                
                 <span className="card-infos sub">
-                  {localize("Containers.realy_delete_container", locale)}
+                  {localize("Containers.realy_delete_container", locale)}  
+                            
                 </span>
+
               </div>
+              <div className="input-field col s12">
+          <input
+            name="groupDelCont"
+            type="checkbox"
+            id="delCont"
+            className="checkbox-red"
+            checked={deleteCertificate}
+            onClick={this.toggleDeleteCertificate}
+          />
+          <label htmlFor="delCont">{localize("Certificate.delete_certificate_and_container", locale)}</label>
+        </div>
             </div>
           </div>
         </div>
@@ -51,6 +91,7 @@ class ContainerDelete extends React.Component<IContainerDeleteProps, {}> {
       </React.Fragment>
     );
   }
+ 
 
   handelCancel = () => {
     const { onCancel } = this.props;
@@ -60,11 +101,15 @@ class ContainerDelete extends React.Component<IContainerDeleteProps, {}> {
     }
   }
 
+  toggleDeleteCertificate = () => {
+    this.setState({ deleteCertificate: !this.state.deleteCertificate });
+  }
+
   handleRemove = () => {
     // tslint:disable-next-line:no-shadowed-variable
     const { container, reloadContainers } = this.props;
     const { localize, locale } = this.context;
-
+    const { certificate, deleteCertificate } = this.state;
     if (!container) {
       return;
     }
@@ -87,6 +132,16 @@ class ContainerDelete extends React.Component<IContainerDeleteProps, {}> {
         userName: USER_NAME,
       });
 
+      if(deleteCertificate == true)
+      {
+        if (!window.PKISTORE.deleteCertificate(container.certificate)) {
+          $(".toast-cert_delete_failed").remove();
+          Materialize.toast(localize("Certificate.cert_delete_failed", locale), 2000, "toast-cert_delete_failed");
+    
+          return;
+        }
+      }
+
       reloadContainers();
     } catch (err) {
       $(".toast-container_delete_failed").remove();
@@ -104,6 +159,20 @@ class ContainerDelete extends React.Component<IContainerDeleteProps, {}> {
         userName: USER_NAME,
       });
     }
+  }
+  getCertificateByContainer = (container: any) => {
+    let certificate = "";
+
+    if (container.category === "MY" && container.key) {
+      try {
+        const x509 = window.PKISTORE.getPkiObject(container);
+        container = trusted.utils.Csp.getContainerNameByCertificate(x509);
+      } catch (e) {
+        // console.log("error get container by certificate", e);
+      }
+    }
+
+    return container;
   }
 }
 
