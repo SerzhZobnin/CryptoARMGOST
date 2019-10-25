857f5e9a72c551136de8f2bc5fe6973331b63496
Aleksandr Gavrilov <alg@digt.ru>
2019-10-25 12:41:36 +0300

    bump version to 2.1.0

--- app/i18n/EN.ts
+++ app/i18n/EN.ts
@@ -41,9 +41,9 @@ export default {
     send: "Send",
     support: "Support",
     username: "Name",
-    version: "2.0.1",
+    version: "2.1.0",
     version_app: "Application version",
-    version_full: "Version: 2.0.1",
+    version_full: "Version: 2.1.0",
   },
   Agreement: {
     Agreement: "AGREEMENT",
