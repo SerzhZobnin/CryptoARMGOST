33c32bcd5b33b8a4c28c555ba64894ebd396e161
Maxim Kanashin <kam@digt.ru>
2019-11-07 16:09:35 +0300

    add buildDocumentPackageDSS
    https://git.digtlab.ru/trusted.ru/cryptoarmgost/issues/252

--- app/utils/dss/helpers.ts
+++ app/utils/dss/helpers.ts
@@ -2,24 +2,40 @@ import * as fs from "fs";
 import * as path from "path";
 import { DSS_ACTIONS, SIGNATURE_TYPE } from "../../constants";
 
-export function buildTransaction(pathDocument: string, certificateId: number,
+export function buildTransaction(document: string | IDocumentContent[], certificateId: number,
                                  isDetached: boolean, operationCode: number) {
 
-  const content = fs.readFileSync(pathDocument, "base64");
-  const body: ITransaction = {
-    Document: content,
-    Documents: [],
-    OperationCode: operationCode,
-    Parameters:
-      [
-        { Name: "SignatureType", Value: "CMS" },
-        { Name: "CertificateID", Value: `${certificateId}` },
-        { Name: "DocumentInfo", Value: path.basename(pathDocument) },
-        { Name: "DocumentType", Value: path.extname(pathDocument).replace(/\./g, "") },
-        { Name: "IsDetached", Value: `${isDetached}` },
-        { Name: "CADESType", Value: "BES" },
-      ],
-  };
+  let body: ITransaction;
+  if (typeof document === "string") {
+    const content = fs.readFileSync(document, "base64");
+    body = {
+      Document: content,
+      Documents: [],
+      OperationCode: operationCode,
+      Parameters:
+        [
+          { Name: "SignatureType", Value: "CMS" },
+          { Name: "CertificateID", Value: `${certificateId}` },
+          { Name: "DocumentInfo", Value: path.basename(document) },
+          { Name: "DocumentType", Value: path.extname(document).replace(/\./g, "") },
+          { Name: "IsDetached", Value: `${isDetached}` },
+          { Name: "CADESType", Value: "BES" },
+        ],
+    };
+  } else {
+    body = {
+      Document: "",
+      Documents: document,
+      OperationCode: operationCode,
+      Parameters:
+        [
+          { Name: "SignatureType", Value: "CMS" },
+          { Name: "CertificateID", Value: `${certificateId}` },
+          { Name: "IsDetached", Value: `${isDetached}` },
+          { Name: "CADESType", Value: "BES" },
+        ],
+    };
+  }
 
   return body;
 }
@@ -40,6 +56,26 @@ export function buildDocumentDSS(pathDocument: string, certificateId: number,
         CmsSignatureType: cmsSignatureType ? cmsSignatureType : "sign",
         IsDetached: `${isDetached}`,
         OriginalDocument: originalDocument,
+      },
+      PinCode: "",
+      Type: SIGNATURE_TYPE.CMS,
+    },
+  };
+
+  return body;
+}
+
+export function buildDocumentPackageDSS(documents: IDocumentContent[], certificateId: number,
+                                        isDetached: boolean, cmsSignatureType?: string) {
+
+  const body: IDocumentPackageDSS = {
+    Documents: documents,
+    Signature: {
+      CertificateId: certificateId,
+      Parameters: {
+        CADESType: "BES",
+        CmsSignatureType: cmsSignatureType ? cmsSignatureType : "Sign", // Cosign
+        IsDetached: `${isDetached}`,
       },
       PinCode: "",
       Type: SIGNATURE_TYPE.CMS,
