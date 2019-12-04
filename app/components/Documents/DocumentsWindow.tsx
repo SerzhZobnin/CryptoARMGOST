564381281c194652be80792afe69c34873c6be12
Aleksandr Gavrilov <alg@digt.ru>
2019-12-04 16:23:48 +0300

    fix block height

--- app/components/Documents/DocumentsWindow.tsx
+++ app/components/Documents/DocumentsWindow.tsx
@@ -210,7 +210,7 @@ class DocumentsWindow extends React.Component<IDocumentsWindowProps, IDocumentsW
                     <div className="primary-text">{localize("Sign.sign_info", locale)}</div>
                     <hr />
                   </div>
-                  <div style={{ height: "calc(100vh - 100px)" }}>
+                  <div style={{ height: "calc(100vh - 80px)" }}>
                     <div className="add-certs">
                       <SignatureInfoBlock
                         signatures={fileSignatures}
