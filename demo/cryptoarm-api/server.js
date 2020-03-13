4166a30b2a7829254d08d4f112c431fb9d3be87d
Aleksandr Gavrilov <alg@digt.ru>
2020-03-13 16:38:18 +0300

    add some jsons examples
    https://git.digtlab.ru/trusted.ru/cryptoarmgost/issues/474

--- demo/cryptoarm-api/server.js
+++ demo/cryptoarm-api/server.js
@@ -1,22 +1,40 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const morgan = require('morgan');
+
 const app = express();
 
 const PORT = 8080;
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.use('/form', express.static(__dirname + '/index.html'));
+app.get('/form', function (req, res) {
+  res.sendFile(__dirname + '/form.html');
+});
 
 // default options
 app.use(fileUpload());
+app.use(morgan('tiny'));
 
-app.use('/static', express.static(__dirname + '/public'));
-
-app.get('/ping', function (req, res) {
-  res.send('pong');
-});
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function (req, res) {
-  res.json({ user: 'tobi' });
+  const accessToken = req.query.accessToken;
+
+  switch (accessToken) {
+    case "efac5229-0595-41e9-b1aa-7df406539b87":
+      res.sendFile(__dirname + '/public/json/efac5229-0595-41e9-b1aa-7df406539b87.json');
+      break;
+    case "9c7101f7-9c47-4481-b4da-a6a497abde08":
+      res.sendFile(__dirname + '/public/json/9c7101f7-9c47-4481-b4da-a6a497abde08.json');
+      break;
+    case "2c48eb32-a0a8-405c-ade9-eed130605cba":
+      res.sendFile(__dirname + '/public/json/2c48eb32-a0a8-405c-ade9-eed130605cba.json');
+      break;
+    default:
+      res.status(400).send('Incorrect token');
+  }
 });
 
 app.post('/upload', function (req, res) {
@@ -30,7 +48,7 @@ app.post('/upload', function (req, res) {
 
   console.log('req.files >>>', req.files);
 
-  sampleFile = req.files.sampleFile;
+  sampleFile = req.files.file;
 
   uploadPath = __dirname + '/uploads/' + sampleFile.name;
 
