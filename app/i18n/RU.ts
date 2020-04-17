8933a8c85e201598d381f7afb9ea82a2a0238c12
Aleksandr Gavrilov <alg@digt.ru>
2020-04-17 12:14:31 +0300

    bump version to 2.4.1

--- app/i18n/RU.ts
+++ app/i18n/RU.ts
@@ -41,9 +41,9 @@ export default {
     send: "Отправить",
     support: "Техническая поддержка",
     username: "Имя",
-    version: "2.4.0",
+    version: "2.4.1",
     version_app: "Версия приложения",
-    version_full: "Версия продукта: 2.4.0",
+    version_full: "Версия продукта: 2.4.1",
   },
   AddressBook: {
     address_book: "Контакты",
