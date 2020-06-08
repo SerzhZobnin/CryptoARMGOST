9632a820f43de034af839adb5c3d8d893061d4ce
Aleksandr Gavrilov <alg@digt.ru>
2020-06-08 17:28:58 +0300

    bump version to 2.5.1

--- app/i18n/EN.ts
+++ app/i18n/EN.ts
@@ -41,9 +41,9 @@ export default {
     send: "Send",
     support: "Support",
     username: "Name",
-    version: "2.5.0",
+    version: "2.5.1",
     version_app: "Application version",
-    version_full: "Version: 2.5.0",
+    version_full: "Version: 2.5.1",
   },
   AddressBook: {
     address_book: "Address book",
